36077eafaf10e589e0c3bb117e630fd82b45d915
YarosJ <jaroslawjuk@gmail.com>
2018-11-09 15:28:14 +0200

    Changed imports to require :ambulance:

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import makeScreenshot from './lib/makeScreenshot';
-import getSelector from './lib/getSelector';
+const makeScreenshot = require('./lib/makeScreenshot');
+const getSelector = require('./lib/getSelector');
 
-module.exports = { makeScreenshot, getSelector};
+module.exports = { makeScreenshot, getSelector };
+
+// const result = await getSelector('https://www.npmjs.com', [50, 120])
+//  const result = await makeScreenshot('https://www.npmjs.com');
